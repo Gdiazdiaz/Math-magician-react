de901bc4b1d89a99f5c17a01404cbf92354d5325
Glenda Diaz <glendadiazah@outlook.com>
2023-01-18 14:27:29 -0600

    Add router to home, calculator and quote. Add style

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -15,41 +15,44 @@ const Calculator = () => {
     total, next, operation, error,
   } = result;
   return (
-    <div className="calculator-container">
-      <div className="result">
-        {total}
-        {operation}
-        {next}
-        {error}
-      </div>
-      <div className="line">
-        <button type="button" className="key" value="AC" onClick={click}>AC</button>
-        <button type="button" className="key" value="+/-" onClick={click}>+/-</button>
-        <button type="button" className="key" value="%" onClick={click}>%</button>
-        <button type="button" className="key color" value="÷" onClick={click}>÷</button>
-      </div>
-      <div className="line">
-        <button type="button" className="key" value="7" onClick={click}>7</button>
-        <button type="button" className="key" value="8" onClick={click}>8</button>
-        <button type="button" className="key" value="9" onClick={click}>9</button>
-        <button type="button" className="key color" value="x" onClick={click}>x</button>
-      </div>
-      <div className="line">
-        <button type="button" className="key" value="4" onClick={click}>4</button>
-        <button type="button" className="key" value="5" onClick={click}>5</button>
-        <button type="button" className="key" value="6" onClick={click}>6</button>
-        <button type="button" className="key color" value="-" onClick={click}>-</button>
-      </div>
-      <div className="line">
-        <button type="button" className="key" value="1" onClick={click}>1</button>
-        <button type="button" className="key" value="2" onClick={click}>2</button>
-        <button type="button" className="key" value="3" onClick={click}>3</button>
-        <button type="button" className="key color" value="+" onClick={click}>+</button>
-      </div>
-      <div className="line">
-        <button type="button" className="key-exception" value="0" onClick={click}>0</button>
-        <button type="button" className="key" value="." onClick={click}>.</button>
-        <button type="button" className="key color" value="=" onClick={click}>=</button>
+    <div className="main-container">
+      <h2 className="calculator-title">Let`s Do Some Math</h2>
+      <div className="calculator-container">
+        <div className="result">
+          {total}
+          {operation}
+          {next}
+          {error}
+        </div>
+        <div className="line">
+          <button type="button" className="key" value="AC" onClick={click}>AC</button>
+          <button type="button" className="key" value="+/-" onClick={click}>+/-</button>
+          <button type="button" className="key" value="%" onClick={click}>%</button>
+          <button type="button" className="key color" value="÷" onClick={click}>÷</button>
+        </div>
+        <div className="line">
+          <button type="button" className="key" value="7" onClick={click}>7</button>
+          <button type="button" className="key" value="8" onClick={click}>8</button>
+          <button type="button" className="key" value="9" onClick={click}>9</button>
+          <button type="button" className="key color" value="x" onClick={click}>x</button>
+        </div>
+        <div className="line">
+          <button type="button" className="key" value="4" onClick={click}>4</button>
+          <button type="button" className="key" value="5" onClick={click}>5</button>
+          <button type="button" className="key" value="6" onClick={click}>6</button>
+          <button type="button" className="key color" value="-" onClick={click}>-</button>
+        </div>
+        <div className="line">
+          <button type="button" className="key" value="1" onClick={click}>1</button>
+          <button type="button" className="key" value="2" onClick={click}>2</button>
+          <button type="button" className="key" value="3" onClick={click}>3</button>
+          <button type="button" className="key color" value="+" onClick={click}>+</button>
+        </div>
+        <div className="line">
+          <button type="button" className="key-exception" value="0" onClick={click}>0</button>
+          <button type="button" className="key" value="." onClick={click}>.</button>
+          <button type="button" className="key color" value="=" onClick={click}>=</button>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+/* eslint-disable max-len */
+import React from 'react';
+import './Quote.css';
+
+function Quote() {
+  return (
+    <div className="quote-container">
+      <p className="quote-text">We will always have STEM with us. Some things will drop out of the public eye and go away, but there will always be science, engineering, and technology. And there will always, always be mathematics.</p>
+      <p className="quote-author">— Katherine Johnson, African-American mathematician</p>
+    </div>
+  );
+}
+
+export default Quote;
