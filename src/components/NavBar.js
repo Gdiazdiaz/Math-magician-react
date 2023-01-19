b1b0409e64916b7b38713ff2a17c501b61d95638
Glenda Diaz <glendadiazah@outlook.com>
2023-01-19 02:36:12 -0600

    fix path for home in navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => (
   <div className="navbar-container">
     <h1 className="header-title">Math Magician</h1>
     <ul className="items-container">
-      <li><Link to="/Home" className="item">Home</Link></li>
+      <li><Link to="/" className="item">Home</Link></li>
       <li><Link to="/Calculator" className="item">Calculator</Link></li>
       <li><Link to="/Quote" className="item">Quote</Link></li>
     </ul>
